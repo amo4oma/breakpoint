d59135bc546021872c50d81313bddf051ee10992
Ahmed Obad <63117335+amo4oma@users.noreply.github.com>
2023-06-08 17:16:07 +0800

    updating the page

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-var menu = ['01', '02', '03']
+import data from './data.js'; 
+ 
+var menu = ['01', '02', '03', '04','05']
 
 
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     spaceBetween: 30,
+    autoplay: {
+      delay: 5000, // Time between slide transitions (in milliseconds)
+      disableOnInteraction: false, // Allow autoplay to continue even when the user interacts with the slider
+    },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
@@ -16,3 +22,107 @@ var swiper = new Swiper(".mySwiper", {
         prevEl: ".prev-slide",
       },
   });
+
+  var swiper2 = new Swiper(".mySwiper2", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+
+    navigation: {
+        nextEl: ".sw-left",
+        prevEl: ".sw-right",
+      },
+  });
+
+var rightBtn = $('sw-right').prop('disabled')
+var leftBtn = $('sw-right').prop('disabled')
+
+function rightClickHandler(){
+  if (rightBtn){
+    $('right').attr('src','Assests/arrow-red.png')
+  }
+  console.log('cliked')
+}
+  
+swiper2.on('slideChange', function (e) {
+  if(swiper2.activeIndex === 0){
+    $('.left').attr('src','Assests/arrow-red.png')
+    $('.right').attr('src','Assests/arrow.png')
+  }else if(swiper2.activeIndex === 2){
+    $('.right').attr('src','Assests/arrow-red.png')
+    $('.left').attr('src','Assests/arrow.png')
+
+  }
+
+  console.log('*** mySwiper.activeIndex', swiper2.activeIndex);
+});
+
+ 
+
+$(document).ready(function(){
+  $('#back').hide();
+  $('#book').hide();
+})
+
+$('#back').on('click', function(){
+  $('.description').hide();
+  $('.cards-container').show()
+  $('#back').hide();
+  $('#book').hide();
+  $('.title').text('services');x
+});
+$('#one').on('click', function(){
+  $('#back').show();
+  $('#book').show();
+  $('.cards-container').hide();
+  $('.description').slideDown();
+  $('.description').text(`${data.one}`);
+  $('.title').text('Website development');
+})
+$('#two').on('click', function(){
+  $('#back').show();
+  $('#book').show();
+  $('.cards-container').hide();
+  $('.description').slideDown();
+  $('.description').text(`${data.one}`);
+  $('.title').text('Website development');
+})
+$('#three').on('click', function(){
+  $('#back').show();
+  $('#book').show();
+  $('.cards-container').hide();
+  $('.description').slideDown();
+  $('.description').text(`${data.three}`);
+  $('.title').text('Website development');
+})
+$('#four').on('click', function(){
+  $('#back').show();
+  $('#book').show();
+  $('.cards-container').hide();
+  $('.description').slideDown();
+  $('.description').text(`${data.four}`);
+  $('.title').text('Website development');
+})
+$('#five').on('click', function(){
+  $('#back').show();
+  $('#book').show();
+  $('.cards-container').hide();
+  $('.description').slideDown();
+  $('.description').text(`${data.five}`);
+  $('.title').text('Website development');
+})
+$('#six').on('click', function(){
+  $('#back').show();
+  $('#book').show();
+  $('.cards-container').hide();
+  $('.description').slideDown();
+  $('.description').text(`${data.six}`);
+  $('.title').text('Website development');
+})
+$('#seven').on('click', function(){
+  $('#back').show();
+  $('#book').show();
+  $('.cards-container').hide();
+  $('.description').slideDown();
+  $('.description').text(`${data.seven}`);
+  $('.title').text('Website development');
+})
